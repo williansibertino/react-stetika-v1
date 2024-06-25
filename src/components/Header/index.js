4b822e423265ad76eb36e6e5c19b3a003fc39384
Willks <69859000+williansibertino@users.noreply.github.com>
2024-06-24 23:24:02 -0300

    Criação da tela react front-end

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+import estilos from "./style-header.module.css";
+
+function Header() {
+  return (
+    <div>
+      <header className={estilos.containerHeader}>
+        <img src="./images/logo.svg" alt="logo stetika"/>
+        <nav>
+          <ul className={estilos.menu}>
+            <li><a className={estilos.linkAtivo} href="#">Agenda</a></li>
+            <li><a href="#">Financeiro</a></li>
+            <li><a href="#">Consultas</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
